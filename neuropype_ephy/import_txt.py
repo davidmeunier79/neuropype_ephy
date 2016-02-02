7c70e0e096acfbef33fe4de9b7f9ddff79134f5c
David Meunier (HP-EliteBook) <david.meunier@inserm.fr>
2016-02-02 14:09:42 +0100

    ajout fonctions multiwin : split_txt_multiwin create_pipeline_brain_vision_ascii_to_multiwin_spectral_connectivity

--- neuropype_ephy/import_txt.py
+++ neuropype_ephy/import_txt.py
@@ -38,8 +38,13 @@ def split_txt(sample_size,txt_file,sep_label_name):
 
     if df.shape[1] % sample_size != 0:
 
-        print "Warning, sample_size is not a multiple of ts shape"
+        print "Error, sample_size is not a multiple of ts shape"
 
+        print sample_size
+        print df
+        
+        0/0
+        
         return 
 
     nb_epochs = df.shape[1] / sample_size
@@ -56,6 +61,77 @@ def split_txt(sample_size,txt_file,sep_label_name):
 
     print np_splitted_ts.shape
 
+    splitted_ts_file = os.path.abspath("splitted_ts.npy")
+
+    np.save(splitted_ts_file,np_splitted_ts)
+
+    return splitted_ts_file,elec_names_file
+
+
+def split_txt_multiwin(sample_size,txt_file,t_windows,sep_label_name):
+
+    import os
+
+    import numpy as np
+    import pandas as pd
+
+    df = pd.read_table(txt_file,sep = ";",decimal = ",", header = None, index_col = 0)
+    #df = pd.read_table(txt_file,sep = ";",decimal = ",", header = None, index_col = None)
+    print df.values.shape
+    
+    ## electrode names:
+    np_indexes = df.index.values
+
+    list_indexes = np_indexes.tolist()
+
+    print list_indexes
+
+    if sep_label_name != "" :
+        keep = np.array([len(index.split(sep_label_name)) == 2 for index in list_indexes],dtype = "int")
+        
+    else:
+        keep = np.ones(shape = np_indexes.shape)
+                        
+    print keep
+
+    print np_indexes[keep == 1]
+
+
+    elec_names_file = os.path.abspath("correct_channel_names.txt")
+
+    np.savetxt(elec_names_file,np_indexes[keep == 1],fmt = "%s")
+
+    ## splitting data_path
+    print df.shape
+
+    if df.shape[1] % sample_size != 0:
+
+        print "Error, sample_size is not a multiple of ts shape"
+
+        print sample_size
+        print df
+        
+        0/0
+        
+        return 
+    
+    nb_epochs = df.shape[1] / sample_size
+
+    print nb_epochs
+
+    splitted_ts = np.split(df.values,nb_epochs,axis = 1)
+
+    print len(splitted_ts)
+
+    print splitted_ts[0]
+
+    np_splitted_ts = np.array(splitted_ts,dtype = 'float')
+
+    print np_splitted_ts.shape
+
+    print t_windows
+    0/0
+    
     splitted_ts_file = os.path.abspath("splitted_ts.npy")
 
     np.save(splitted_ts_file,np_splitted_ts)
--- neuropype_ephy/pipelines/brain_vision_to_conmat.py
+++ neuropype_ephy/pipelines/brain_vision_to_conmat.py
@@ -3,7 +3,7 @@
 import nipype.pipeline.engine as pe
 from nipype.interfaces.utility import Function
 
-from neuropype_ephy.import_txt import split_txt
+from neuropype_ephy.import_txt import split_txt,split_txt_multiwin
 
 from neuropype_ephy.spectral import  epoched_spectral_proc,spectral_proc
 from neuropype_ephy.spectral import  plot_circular_connectivity,filter_adj_plot_mat
@@ -69,7 +69,43 @@ def create_pipeline_brain_vision_ascii_to_spectral_connectivity(main_path,con_me
     return pipeline
     
     
+def create_pipeline_brain_vision_ascii_to_multiwin_spectral_connectivity(main_path,t_windows, con_method = "coh", sample_size = 512, sep_label_name = "", sfreq = 512):
+
+    pipeline = pe.Workflow(name="brain_vision_to_multiwin_conmat")
+    pipeline.base_dir = main_path
     
+    #### convert
+    split_ascii_multiwin = pe.Node(interface = Function(input_names = ["sample_size","txt_file","sep_label_name","t_windows"],output_names = ["splitted_ts_file","elec_names_file"],function = split_txt_multiwin),name = 'split_ascii_multiwin')
+    
+    split_ascii_multiwin.inputs.t_windows = t_windows
+    split_ascii_multiwin.inputs.sample_size = sample_size
+    split_ascii_multiwin.inputs.sep_label_name = sep_label_name
+    
+    ##### spectral
+    #spectral = pe.Node(interface = Function(input_names = ["ts_file","sfreq","freq_band","con_method"],
+                                            #output_names = "conmat_file",
+                                            #function = spectral_proc),name = "spectral")
+    
+    #spectral.inputs.con_method = con_method    
+    #spectral.inputs.sfreq = sfreq
+    
+    #pipeline.connect(split_ascii, 'splitted_ts_file', spectral, 'ts_file')
+
+    ##### plot spectral
+    #plot_spectral = pe.Node(interface = Function(input_names = ["conmat_file","labels_file"],
+                                                 #output_names = "plot_conmat_file",
+                                                 #function = plot_circular_connectivity), name = "plot_spectral")
+    
+    ## plot_spectral.inputs.labels_file = MEG_elec_names_file AP 021015
+    #plot_spectral.inputs.nb_lines = 200
+    
+    #pipeline.connect(split_ascii,  'elec_names_file',plot_spectral,'labels_file')
+    #pipeline.connect(spectral, "conmat_file",    plot_spectral, 'conmat_file')
+    
+    return pipeline
+    
+    
+        
     
     
     
