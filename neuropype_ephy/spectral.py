e306bc5aea29d0d04ab886daa4d14ff77ac3cfea
Annalisa Pascarella <a.pascarella@iac.cnr.it>
2016-03-18 14:37:41 -0400

    new plot_circular_connectivity

--- neuropype_ephy/spectral.py
+++ neuropype_ephy/spectral.py
@@ -411,47 +411,47 @@ def split_win_ts(splitted_ts_file,n_windows):
         
 ########################################################### plot spectral connectivity #################################################################
 
-def plot_circular_connectivity(conmat_file,labels_file,nb_lines, vmin = None, vmax = None):
-
-    import os
-    
-    import numpy as np
-    
-    from nipype.utils.filemanip import split_filename as split_f
-    
-    from mne.viz import circular_layout, plot_connectivity_circle
-    import matplotlib.pyplot as plt
-    
-    label_names= [line.strip() for line in open(labels_file)]
-    
-    path,fname,ext = split_f(conmat_file)
-    
-    print fname
-    
-    
-    #print label_names
-    conmat = np.load(conmat_file)
-    #print conmat.shape
-    
-    # Angles
-    node_angles = circular_layout(label_names, node_order = label_names, start_pos=90,
-                                group_boundaries=[0, len(label_names) / 2])
-
-    # Plot the graph using node colors from the FreeSurfer parcellation. We only
-    # show the 300 strongest connections.
-    fig,_ = plot_connectivity_circle(conmat, label_names, n_lines=nb_lines,  node_angles=node_angles, fontsize_names = 12, title='All-to-All Connectivity' , show = False, vmin = vmin, vmax = vmax)
-    
-    
-    #plot_conmat_file = os.path.abspath('circle.png')
-    plot_conmat_file = os.path.abspath('circle_' + fname + '.eps')
-    fig.savefig(plot_conmat_file, facecolor='black')
-    
-    
-    plt.close(fig)
-    #fig1.close()
-    del fig
-    
-    return plot_conmat_file
+#def plot_circular_connectivity(conmat_file,labels_file,nb_lines, vmin = None, vmax = None):
+#
+#    import os
+#    
+#    import numpy as np
+#    
+#    from nipype.utils.filemanip import split_filename as split_f
+#    
+#    from mne.viz import circular_layout, plot_connectivity_circle
+#    import matplotlib.pyplot as plt
+#    
+#    label_names= [line.strip() for line in open(labels_file)]
+#    
+#    path,fname,ext = split_f(conmat_file)
+#    
+#    print fname
+#    
+#    
+#    #print label_names
+#    conmat = np.load(conmat_file)
+#    #print conmat.shape
+#    
+#    # Angles
+#    node_angles = circular_layout(label_names, node_order = label_names, start_pos=90,
+#                                group_boundaries=[0, len(label_names) / 2])
+#
+#    # Plot the graph using node colors from the FreeSurfer parcellation. We only
+#    # show the 300 strongest connections.
+#    fig,_ = plot_connectivity_circle(conmat, label_names, n_lines=nb_lines,  node_angles=node_angles, fontsize_names = 12, title='All-to-All Connectivity' , show = False, vmin = vmin, vmax = vmax)
+#    
+#    
+#    #plot_conmat_file = os.path.abspath('circle.png')
+#    plot_conmat_file = os.path.abspath('circle_' + fname + '.eps')
+#    fig.savefig(plot_conmat_file, facecolor='black')
+#    
+#    
+#    plt.close(fig)
+#    #fig1.close()
+#    del fig
+#    
+#    return plot_conmat_file
 
      
 def filter_adj_plot_mat(conmat_file,labels_file,sep_label_name,k_neigh):
