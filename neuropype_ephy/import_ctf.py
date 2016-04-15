ce3c6538414ae97938ce47170b1c0559bbbf9803
Dmitrii Altukhov <dmalt@me.com>
2016-04-15 18:15:45 -0400

    add check on raw_fif_file

--- neuropype_ephy/import_ctf.py
+++ neuropype_ephy/import_ctf.py
@@ -16,13 +16,23 @@ from params import main_path
 
 def convert_ds_to_raw_fif(ds_file):
     import os
+    import os.path as op
+    
     from nipype.utils.filemanip import split_filename as split_f
     from mne.io import read_raw_ctf
+
     subj_path,basename,ext = split_f(ds_file)
     print subj_path,basename,ext
-    raw = read_raw_ctf(ds_file)
-    raw_fif_file = os.path.abspath(basename + "_raw.fif")
-    raw.save(raw_fif_file)
+    
+    #    raw_fif_file = os.path.abspath(basename + "_raw.fif")
+    raw_fif_file = os.path.join(subj_path,basename + "_raw.fif")
+    
+    if not op.isfile(raw_fif_file):       
+        raw = read_raw_ctf(ds_file)
+        raw.save(raw_fif_file)
+    else:      
+        print '*** RAW FIF file %s exists!!!' % raw_fif_file
+    
     return raw_fif_file
 
 
