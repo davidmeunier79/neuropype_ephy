7aa9a5b7d19e5c5aa29076db0862870ad9806637
Dmitrii Altukhov <dmalt@me.com>
2016-04-15 18:16:45 -0400

    add functions to create and read noise_cov add cov_fname and aseg as input of compute_ROIs_inv_sol

--- neuropype_ephy/compute_inv_problem.py
+++ neuropype_ephy/compute_inv_problem.py
@@ -5,8 +5,62 @@ Created on Thu Oct  8 17:53:07 2015
 
 @author: pasca
 """
+
+
+# compute noise covariance data from a continuous segment of raw data.
+# Employ empty room data (collected without the subject) to calculate the full noise covariance matrix. 
+# This is recommended for analyzing ongoing spontaneous activity. 
+def compute_noise_cov(cov_fname, raw):
+    import os.path as op
+    import numpy as np
+    
+    from mne import compute_raw_covariance, pick_types, write_cov, read_cov
+    from nipype.utils.filemanip import split_filename as split_f
+    
+    print '***** COMPUTE RAW COV *****' + cov_fname
+    
+    if not op.isfile(cov_fname):    
+        
+        data_path, basename, ext = split_f(raw.info['filename'])
+        fname = op.join(data_path, '%s-cov.fif' % basename)
+                
+        reject = dict(mag=4e-12, grad=4000e-13, eog=250e-6)
+    
+        picks = pick_types(raw.info, meg=True, ref_meg=False, exclude='bads')
+        
+        noise_cov = compute_raw_covariance(raw, picks=picks, reject=reject)
+
+        write_cov(fname, noise_cov)
+        
+    else:
+        print '*** NOISE cov file %s exists!!!' % cov_fname
+#        noise_cov = read_cov(cov_fname)
+    
+#    return noise_cov
+    return cov_fname
+    
+def read_noise_cov(cov_fname, raw_info):
+    import os.path as op
+    import numpy as np
+    import mne    
+
+    print '***** READ RAW COV *****' + cov_fname
+   
+    if not op.isfile(cov_fname):    
+        # create an Identity matrix
+        picks = mne.pick_types(raw_info, meg=True, ref_meg=False, exclude='bads')
+        ch_names=[raw_info['ch_names'][i] for i in picks]
+
+        C = mne.Covariance(data=np.identity(len(picks)), names=ch_names, bads = [], projs=[], nfree=0)
+        mne.write_cov(cov_fname, C)
+    else:
+        print '*** noise covariance file %s exists!!!' % cov_fname
+        noise_cov = mne.read_cov(cov_fname)
+    
+    return noise_cov
+
 # compute inverse solution on raw data
-def compute_inv_sol(raw, fwd_filename, snr, inv_method):
+def compute_inv_sol(raw, fwd_filename, cov_fname, snr, inv_method, aseg):
     import os.path as op
     import numpy as np
     import mne
@@ -17,18 +71,10 @@ def compute_inv_sol(raw, fwd_filename, snr, inv_method):
     forward = mne.read_forward_solution(fwd_filename)
     
     # Convert to surface orientation for cortically constrained inverse modeling
-    forward = mne.convert_forward_solution(forward, surf_ori=True)
+    if not aseg:
+        forward = mne.convert_forward_solution(forward, surf_ori=True)
 
     lambda2 = 1.0 / snr ** 2
-    reject = dict(mag=4e-12, grad=4000e-13, eog=250e-6)
-
-    picks = mne.pick_types(raw.info, meg=True, ref_meg=False, exclude='bads')
-
-    # compute noise covariance data from a continuous segment of raw data.
-    # Employ empty room data (collected without the subject) to calculate the full noise covariance matrix. 
-    # This is recommended for analyzing ongoing spontaneous activity. 
-    print '***** COMPUTE RAW COV *****'
-    noise_cov = mne.compute_raw_covariance(raw, picks=picks, reject=reject)
     
     # compute inverse operator    
     print '***** COMPUTE INV OP *****'
@@ -62,30 +108,28 @@ def compute_inv_sol(raw, fwd_filename, snr, inv_method):
     
 # compute the inverse solution on raw data considering N_r regions in source space 
 # based on a FreeSurfer cortical parcellation
-def compute_ROIs_inv_sol(raw, sbj_id, sbj_dir, fwd_filename, snr, inv_method, parc):
+def compute_ROIs_inv_sol(raw, sbj_id, sbj_dir, fwd_filename, cov_fname, snr, inv_method, parc, aseg):
     import os.path as op
     import numpy as np
     import mne
     from mne.minimum_norm import make_inverse_operator, apply_inverse_raw
     from nipype.utils.filemanip import split_filename as split_f
+     
+    print '***** READ noise covariance %s *****' % cov_fname
+    noise_cov = mne.read_cov(cov_fname)
     
     print '***** READ FWD SOL %s *****' %fwd_filename
     forward = mne.read_forward_solution(fwd_filename)
-    forward = mne.convert_forward_solution(forward, surf_ori=True)
+    
+    if not aseg:
+        forward = mne.convert_forward_solution(forward, surf_ori=True)
 
     lambda2 = 1.0 / snr ** 2
-    reject = dict(mag=4e-12, grad=4000e-13, eog=250e-6)
-
-    picks = mne.pick_types(raw.info, meg=True, ref_meg=False, exclude='bads')
-
-    # compute noise covariance data
-    print '***** COMPUTE RAW COV *****'
-    noise_cov = mne.compute_raw_covariance(raw, picks=picks, reject=reject)
     
     # compute inverse operator    
-    print '***** COMPUTE INV OP *****'
+    print '***** COMPUTE INV OP *****'    
     inverse_operator = make_inverse_operator(raw.info, forward, noise_cov, loose=0.2, depth=0.8)
-    
+ 
     # apply inverse operator to the time windows [t_start, t_stop]s
     print '***** APPLY INV OP *****' 
     stc = apply_inverse_raw(raw, inverse_operator, lambda2, inv_method, label=None, 
