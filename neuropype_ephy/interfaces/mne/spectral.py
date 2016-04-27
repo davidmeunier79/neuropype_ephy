62e3c2ac16d5d7bfc27a46fedd1cb1c894f4c776
Your Name <you@example.com>
2016-04-27 17:49:49 -0400

    ajout epoch_window_length et choix de la fonction (A modifier a moyen terme)

--- neuropype_ephy/interfaces/mne/spectral.py
+++ neuropype_ephy/interfaces/mne/spectral.py
@@ -11,16 +11,11 @@ from nipype.interfaces.base import BaseInterface, \
     
 ############################################################################################### SpectralConn #####################################################################################################
 
-
-Function(input_names = ["ts_file","sfreq","freq_band","con_method"],
-                                                output_names = "conmat_file",
-                                                function = spectral_proc),
-        
-from neuropype_ephy.spectral import spectral_proc
+from neuropype_ephy.spectral import spectral_proc,epoched_spectral_proc
 
 class SpectralConnInputSpec(BaseInterfaceInputSpec):
     
-    ts_file = traits.List(File(exists=True), desc='nodes * time series in .npy format', mandatory=True)
+    ts_file = traits.File(exists=True, desc='nodes * time series in .npy format', mandatory=True)
     
     sfreq = traits.Float(desc='sampling frequency', mandatory=True)
     
@@ -28,6 +23,7 @@ class SpectralConnInputSpec(BaseInterfaceInputSpec):
     
     con_method = traits.Enum("coh","imcoh","plv","pli",desc='metric computed on time series for connectivity')
     
+    epoch_window_length = traits.Float(desc='epoched data', mandatory=False)
     
 class SpectralConnOutputSpec(TraitedSpec):
     
@@ -48,8 +44,13 @@ class SpectralConn(BaseInterface):
         sfreq = self.inputs.sfreq
         freq_band = self.inputs.freq_band
         con_method = self.inputs.con_method
+        epoch_window_length = self.inputs.epoch_window_length
         
-        self.conmat_file = spectral_proc(ts_file,sfreq,freq_band,con_method)
+        if epoch_window_length == traits.Undefined:
+            self.conmat_file = spectral_proc(ts_file,sfreq,freq_band,con_method)
+        
+        else:
+            self.conmat_file = epoched_spectral_proc(ts_file,sfreq,freq_band,con_method,epoch_window_length)
 
         return runtime
         
@@ -57,7 +58,7 @@ class SpectralConn(BaseInterface):
         
         outputs = self._outputs().get()
         
-        outputs["conmat_file"] = conmat_file
+        outputs["conmat_file"] = self.conmat_file
         
         return outputs
         
