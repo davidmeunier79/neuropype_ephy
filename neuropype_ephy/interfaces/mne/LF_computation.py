de0d99dee4c074ee33682fbd51db09f8d289ceaa
Dmitrii Altukhov <dmalt@me.com>
2016-05-03 12:33:45 -0400

    added files Inverse_solution LF_computation fed_computation

---
+++ neuropype_ephy/interfaces/mne/LF_computation.py
@@ -0,0 +1,109 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon May  2 17:24:00 2016
+
+@author: pasca
+"""
+
+# -*- coding: utf-8 -*-
+import os.path as op
+
+from nipype.utils.filemanip import split_filename as split_f
+
+from nipype.interfaces.base import BaseInterface, BaseInterfaceInputSpec
+from nipype.interfaces.base import traits, File, TraitedSpec
+
+from neuropype_ephy.compute_fwd_problem import create_mixed_source_space
+from neuropype_ephy.compute_fwd_problem import create_bem_sol, create_src_space
+from neuropype_ephy.compute_fwd_problem import is_trans, compute_fwd_sol
+
+
+class LFComputationConnInputSpec(BaseInterfaceInputSpec):
+
+    sbj_id = traits.String(desc='subject id', mandatory=True)
+
+    sbj_dir = traits.Directory(exists=True, desc='Freesurfer main directory',
+                               mandatory=True)
+
+    raw_info = traits.Any(desc='raw info', mandatory=True)
+
+    aseg = traits.Bool(desc='if true sub structures will be considered',
+                       mandatory=False)
+
+    spacing = traits.String(desc='spacing to use to setup a source space',
+                            mandatory=False)
+
+    aseg_labels = traits.List(desc='list of substructures in the src space',
+                              mandatory=False)
+
+
+class LFComputationConnOutputSpec(TraitedSpec):
+
+    fwd_filename = File(exists=False, desc='LF matrix')
+
+
+class LFComputation(BaseInterface):
+    """
+    Compute LF matrix using MNE Python functions
+    """
+    input_spec = LFComputationConnInputSpec
+    output_spec = LFComputationConnOutputSpec
+
+    def _get_fwd_filename(self, raw_info, aseg, spacing):
+
+        data_path, raw_fname, ext = split_f(raw_info['filename'])
+
+        if aseg == traits.Undefined:
+            fwd_filename = op.join(data_path, '%s-%s-fwd.fif'
+                                   % (raw_fname, spacing))
+        else:
+            fwd_filename = op.join(data_path, '%s-%s-aseg-fwd.fif'
+                                   % (raw_fname, spacing))
+
+        print '*** fwd_filename %s ***' % fwd_filename
+        return fwd_filename
+
+    def _run_interface(self, runtime):
+
+        sbj_id = self.inputs.sbj_id
+        sbj_dir = self.inputs.sbj_dir
+        raw_info = self.inputs.raw_info
+        aseg = self.inputs.aseg
+        spacing = self.inputs.spacing
+        aseg_labels = self.inputs.aseg_labels
+
+        if aseg == traits.Undefined:
+            self.fwd_filename = self._get_fwd_filename(raw_info, aseg, spacing)
+        else:
+            self.fwd_filename = self._get_fwd_filename(raw_info, aseg, spacing)
+
+        # check if we have just created the fwd matrix
+        if not op.isfile(self.fwd_filename):
+            bem = create_bem_sol(sbj_dir, sbj_id)  # bem solution
+
+            src = create_src_space(sbj_dir, sbj_id, spacing)  # src space
+
+            if aseg != traits.Undefined:
+                src = create_mixed_source_space(sbj_dir, sbj_id, spacing,
+                                                aseg_labels, src)
+
+            n = sum(src[i]['nuse'] for i in range(len(src)))
+            print('il src space contiene %d spaces e %d vertici'
+                  % (len(src), n))
+
+            trans_fname = is_trans(raw_info)
+
+            # TODO: ha senso una funzione con un solo cmd?
+            compute_fwd_sol(raw_info, trans_fname, src, bem, self.fwd_filename)
+        else:
+            print '*** FWD file %s exists!!!' % self.fwd_filename
+
+        return runtime
+
+    def _list_outputs(self):
+
+        outputs = self._outputs().get()
+
+        outputs['fwd_filename'] = self.fwd_filename
+
+        return outputs
---
+++ neuropype_ephy/pipelines/fwd_computation.py
@@ -0,0 +1,111 @@
+# -*- coding: utf-8 -*-
+
+import os
+import nipype.pipeline.engine as pe
+import nipype.interfaces.io as nio
+
+from nipype.interfaces.utility import IdentityInterface
+from neuropype_ephy.interfaces.mne.LF_computation import LFComputation
+from neuropype_ephy.pipelines.preproc_meeg import create_pipeline_preproc_meeg
+
+
+def create_pipeline_LF_computation(main_path, sbj_dir,
+                                   pipeline_name='LF_pipeline',
+                                   spacing='ico-5',
+                                   aseg=False,
+                                   aseg_labels=[]):
+
+    pipeline = pe.Workflow(name=pipeline_name)
+    pipeline.base_dir = main_path
+
+    inputnode = pe.Node(IdentityInterface(fields=['sbj_id', 'raw_info']),
+                        name='inputnode')
+
+    LF_computation = pe.Node(interface=LFComputation(), name="LF_computation")
+
+    LF_computation.inputs.sbj_dir = sbj_dir
+    LF_computation.inputs.spacing = spacing
+    if aseg:
+        LF_computation.inputs.aseg = aseg
+        LF_computation.inputs.aseg_labels = aseg_labels
+
+    pipeline.connect(inputnode, 'sbj_id', LF_computation, 'sbj_id')
+    pipeline.connect(inputnode, 'raw_info', LF_computation, 'raw_info')
+
+    return pipeline
+
+
+def get_raw_info(raw):
+    return raw.info
+
+if __name__ == '__main__':
+    main_path = '/home/karim/Documents/Fanny'
+    data_path = main_path
+    sbj_dir = os.path.join(main_path, 'FSF')
+    subject_ids = ['S01']
+    sessions = ['repos_1']  # 'repos_2'
+
+    aseg_labels = ['Left-Accumbens-area',
+                   'Left-Amygdala',
+                   'Left-Caudate',
+                   'Left-Hippocampus',
+                   'Left-Pallidum',
+                   'Left-Putamen',
+                   'Left-Thalamus-Proper',
+                   'Left-Cerebellum-Cortex',
+                   'Brain-Stem',
+                   'Right-Accumbens-area',
+                   'Right-Amygdala',
+                   'Right-Caudate',
+                   'Right-Hippocampus',
+                   'Right-Pallidum',
+                   'Right-Putamen',
+                   'Right-Thalamus-Proper',
+                   'Right-Cerebellum-Cortex']
+
+    # create main workflow
+    main_workflow = pe.Workflow(name='wf_LF_computation')
+    main_workflow.base_dir = main_path
+
+    # info source
+    infosource = pe.Node(interface=IdentityInterface(fields=['subject_id',
+                                                             'sess_index']),
+                         name="infosource")
+    infosource.iterables = [('subject_id', subject_ids),
+                            ('sess_index', sessions)]
+
+    # data source
+    datasource = pe.Node(interface=nio.DataGrabber(infields=['subject_id',
+                                                             'sess_index'],
+                                                   outfields=['raw_file']),
+                         name='datasource')
+    datasource.inputs.base_directory = data_path
+    datasource.inputs.template = '%s/%s%s'
+    datasource.inputs.template_args = dict(raw_file=[['subject_id',
+                                                     'sess_index', ".ds"]])
+    datasource.inputs.sort_filelist = True
+
+    main_workflow.connect(infosource, 'subject_id', datasource, 'subject_id')
+    main_workflow.connect(infosource, 'sess_index', datasource, 'sess_index')
+
+    preproc_workflow = create_pipeline_preproc_meeg(main_path,
+                                                    is_sensor_space=False,
+                                                    data_type='ds')
+
+    main_workflow.connect(datasource, 'raw_file',
+                          preproc_workflow, 'inputspec.raw_file')
+
+    LF_workflow = create_pipeline_LF_computation(main_path, sbj_dir,
+                                                 aseg=True,
+                                                 aseg_labels=aseg_labels)
+
+    main_workflow.connect(infosource, 'subject_id',
+                          LF_workflow, 'inputnode.sbj_id')
+
+    main_workflow.connect(preproc_workflow, ('preproc.ts_file', get_raw_info),
+                          LF_workflow, 'inputnode.raw_info')
+                          
+    # run pipeline
+    main_workflow.write_graph(graph2use='colored')  # colored
+    main_workflow.config['execution'] = {'remove_unnecessary_outputs': 'false'}
+    main_workflow.run(plugin='MultiProc', plugin_args={'n_procs': 8})
