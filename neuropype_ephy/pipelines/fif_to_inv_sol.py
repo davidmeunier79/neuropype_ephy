878659ed0cee077925e2e478e02ccab221b9dc49
Dmitrii Altukhov <dmalt@me.com>
2016-05-05 11:02:09 -0400

    add InverseSolution Node in create_pipeline_source_reconstruction

--- neuropype_ephy/pipelines/fif_to_inv_sol.py
+++ neuropype_ephy/pipelines/fif_to_inv_sol.py
@@ -7,12 +7,15 @@ import nipype.interfaces.io as nio
 from nipype.interfaces.utility import IdentityInterface
 from neuropype_ephy.interfaces.mne.LF_computation import LFComputation
 from neuropype_ephy.interfaces.mne.Inverse_solution import NoiseCovariance
+from neuropype_ephy.interfaces.mne.Inverse_solution import InverseSolution
 from neuropype_ephy.pipelines.preproc_meeg import create_pipeline_preproc_meeg
 
 
 def create_pipeline_source_reconstruction(main_path, sbj_dir,
                                           pipeline_name='inv_sol_pipeline',
                                           spacing='ico-5',
+                                          inv_method='MNE',
+                                          parc='aparc',
                                           aseg=False,
                                           aseg_labels=[],
                                           noise_cov_fname=None):
@@ -23,12 +26,13 @@ def create_pipeline_source_reconstruction(main_path, sbj_dir,
     inputnode = pe.Node(IdentityInterface(fields=['sbj_id', 'raw']),
                         name='inputnode')
 
+    # Lead Field computation Node
     LF_computation = pe.Node(interface=LFComputation(), name='LF_computation')
 
     LF_computation.inputs.sbj_dir = sbj_dir
     LF_computation.inputs.spacing = spacing
+    LF_computation.inputs.aseg = aseg
     if aseg:
-        LF_computation.inputs.aseg = aseg
         LF_computation.inputs.aseg_labels = aseg_labels
 
     pipeline.connect(inputnode, 'sbj_id', LF_computation, 'sbj_id')
@@ -36,6 +40,7 @@ def create_pipeline_source_reconstruction(main_path, sbj_dir,
     pipeline.connect(inputnode, ('raw', get_raw_info),
                      LF_computation, 'raw_info')
 
+    # Noise Covariance Matrix Node
     create_noise_cov = pe.Node(interface=NoiseCovariance(),
                                name="create_noise_cov")
 
@@ -43,6 +48,23 @@ def create_pipeline_source_reconstruction(main_path, sbj_dir,
         create_noise_cov.inputs.cov_fname_in = noise_cov_fname
 
     pipeline.connect(inputnode, 'raw', create_noise_cov, 'raw')
+
+    # Inverse Solution Node
+    inv_solution = pe.Node(interface=InverseSolution(), name='inv_solution')
+
+    inv_solution.inputs.sbj_dir = sbj_dir
+    inv_solution.inputs.inv_method = inv_method
+    inv_solution.inputs.parc = parc
+    inv_solution.inputs.aseg = aseg
+    if aseg:
+        inv_solution.inputs.aseg_labels = aseg_labels
+
+    pipeline.connect(inputnode, 'sbj_id', inv_solution, 'sbj_id')
+    pipeline.connect(inputnode, 'raw', inv_solution, 'raw')
+    pipeline.connect(LF_computation, 'fwd_filename',
+                     inv_solution, 'fwd_filename')
+    pipeline.connect(create_noise_cov, 'cov_fname_out',
+                     inv_solution, 'cov_filename')
 
     return pipeline
 
@@ -54,31 +76,41 @@ if __name__ == '__main__':
     main_path = '/home/karim/Documents/Fanny'
     data_path = main_path
     sbj_dir = os.path.join(main_path, 'FSF')
-    subject_ids = ['S01']
+    subject_ids = ['S01', 'S02']  # 'S02'
     sessions = ['repos_1']  # 'repos_2'
 
     noise_cov_fname = os.path.join(main_path, 'Big_Noise-cov.fif')
 
-    aseg_labels = ['Left-Accumbens-area',
-                   'Left-Amygdala',
-                   'Left-Caudate',
+#    aseg_labels = ['Left-Accumbens-area',
+#                   'Left-Amygdala',
+#                   'Left-Caudate',
+#                   'Left-Hippocampus',
+#                   'Left-Pallidum',
+#                   'Left-Putamen',
+#                   'Left-Thalamus-Proper',
+#                   'Left-Cerebellum-Cortex',
+#                   'Brain-Stem',
+#                   'Right-Accumbens-area',
+#                   'Right-Amygdala',
+#                   'Right-Caudate',
+#                   'Right-Hippocampus',
+#                   'Right-Pallidum',
+#                   'Right-Putamen',
+#                   'Right-Thalamus-Proper',
+#                   'Right-Cerebellum-Cortex']
+
+    aseg_labels = ['Left-Amygdala',
                    'Left-Hippocampus',
-                   'Left-Pallidum',
-                   'Left-Putamen',
                    'Left-Thalamus-Proper',
                    'Left-Cerebellum-Cortex',
                    'Brain-Stem',
-                   'Right-Accumbens-area',
                    'Right-Amygdala',
-                   'Right-Caudate',
                    'Right-Hippocampus',
-                   'Right-Pallidum',
-                   'Right-Putamen',
                    'Right-Thalamus-Proper',
                    'Right-Cerebellum-Cortex']
 
     # create main workflow
-    main_workflow = pe.Workflow(name='wf_LF_computation')
+    main_workflow = pe.Workflow(name='wf_inv_sol_computation')
     main_workflow.base_dir = main_path
 
     # info source
@@ -109,11 +141,14 @@ if __name__ == '__main__':
     main_workflow.connect(datasource, 'raw_file',
                           preproc_workflow, 'inputnode.raw_file')
 
+#    inv_sol_workflow = create_pipeline_source_reconstruction(main_path,
+#                                                             sbj_dir,
+#                                                             aseg=True,
+#                                                             aseg_labels=aseg_labels,
+#                                                             noise_cov_fname=noise_cov_fname)
     inv_sol_workflow = create_pipeline_source_reconstruction(main_path,
                                                              sbj_dir,
                                                              noise_cov_fname=noise_cov_fname)
-#    inv_sol_workflow = create_pipeline_source_reconstruction(main_path,
-#                                                             sbj_dir)
 
     main_workflow.connect(infosource, 'subject_id',
                           inv_sol_workflow, 'inputnode.sbj_id')
