05f8aff9e8cae3332aa4d4e2f84de30aa71fcc61
Dmitriy Altukhov <dm.altukhov@ya.ru>
2016-12-02 13:00:21 -0500

    In fif2ts in pick epochs pick only meg sensors and ignore eeg

--- neuropype_ephy/fif2ts.py
+++ neuropype_ephy/fif2ts.py
@@ -7,13 +7,17 @@ def ep2ts(fif_file):
     timeseries to .npy
     """
     from mne import read_epochs
+    from mne import pick_types
+
     from numpy import save
     import os.path as op
 
     with nostdout():
         epochs = read_epochs(fif_file)
 
-    data = epochs.get_data()
+    epochs_meg = epochs.pick_types(meg=True, eeg=False, eog=False, ecg=False)
+
+    data = epochs_meg.get_data()
     save_path = op.abspath('ts_epochs.npy')
     save(save_path, data)
     return save_path
