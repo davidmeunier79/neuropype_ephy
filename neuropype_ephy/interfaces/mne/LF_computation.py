05e8127d34602236f0a137aac8f773afa6b1f586
Annalisa Pascarella <a.pascarella@iac.cnr.it>
2017-03-20 17:43:41 +0100

    remove is_blind

--- neuropype_ephy/interfaces/mne/LF_computation.py
+++ neuropype_ephy/interfaces/mne/LF_computation.py
@@ -1,11 +1,6 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Mon May  2 17:24:00 2016
+# Created on Mon May  2 17:24:00 2016
+# @author: pasca
 
-@author: pasca
-"""
-
-# -*- coding: utf-8 -*-
 import os.path as op
 
 from nipype.utils.filemanip import split_filename as split_f
@@ -23,14 +18,11 @@ class LFComputationConnInputSpec(BaseInterfaceInputSpec):
     sbj_id = traits.String(desc='subject id', mandatory=True)
 
     sbj_dir = traits.String(exists=True, desc='Freesurfer main directory',
-                               mandatory=True)
+                            mandatory=True)
 
     raw_info = traits.Any(desc='raw info', mandatory=True)
     
     raw_fname = traits.String(desc='raw file name', mandatory=True)
-
-    is_blind = traits.Bool(desc='if in the source space there are ROI removed',
-                           mandatory=False)
 
     spacing = traits.String(desc='spacing to use to setup a source space',
                             mandatory=False)
@@ -54,13 +46,11 @@ class LFComputation(BaseInterface):
     input_spec = LFComputationConnInputSpec
     output_spec = LFComputationConnOutputSpec
 
-    def _get_fwd_filename(self, raw_info, aseg, spacing, is_blind):
+    def _get_fwd_filename(self, raw_info, aseg, spacing):
 
 #        data_path, raw_fname, ext = split_f(raw_info['filename'])
         data_path, raw_fname, ext = split_f(raw_info)
         fwd_filename = '%s-%s' % (raw_fname, spacing)
-        if is_blind:
-            fwd_filename += '-blind'
         if aseg:
             fwd_filename += '-aseg'
 
@@ -76,18 +66,17 @@ class LFComputation(BaseInterface):
         raw_info = self.inputs.raw_info
         raw_fname = self.inputs.raw_fname
         aseg = self.inputs.aseg
-        is_blind = self.inputs.is_blind
         spacing = self.inputs.spacing
         aseg_labels = self.inputs.aseg_labels
 
         self.fwd_filename = self._get_fwd_filename(raw_fname, aseg,
-                                                   spacing, is_blind)
+                                                   spacing)
 
         # check if we have just created the fwd matrix
         if not op.isfile(self.fwd_filename):
             bem = create_bem_sol(sbj_dir, sbj_id)  # bem solution
 
-            src = create_src_space(sbj_dir, sbj_id, spacing, is_blind)  # src space
+            src = create_src_space(sbj_dir, sbj_id, spacing)  # src space
 
             if aseg:
                 src = create_mixed_source_space(sbj_dir, sbj_id, spacing,
